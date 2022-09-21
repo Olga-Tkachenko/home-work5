98992f84de18d7f5c2dde53d761bfd60b201511d
OlgaT <oliunea-t2002@mail.ru>
2022-09-21 19:34:13 +0300

    add zadacha34

---
+++ zadacha34/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте массив заполненный случайными положительными трехзначными числами. 
+// Напишите программу, которая покажет количество четных чисел в массиве.
+// Например: 345, 897, 568, 234 - 2
+int[] createArray()
+{
+var random = new Random();
+
+var result = new int[10];
+
+for (long i = 0; i < result.Length; i++)
+{
+result[i] = random.Next(100, 1000);
+}
+
+return result;
+}
+
+int EvenNumbers(int[] array)
+ {
+var result = 0;
+
+for (long i = 0; i < array.Length; i++)
+{
+if (array[i] %2 == 0)
+result += 1;
+}
+
+return result;
+}
+
+var array = createArray();
+Console.WriteLine("Сгенерирован массив:");
+for(var i = 0; i < array.Length; i++)
+Console.Write(array[i] + ",");
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Количество четных чисел в массиве:");
+Console.WriteLine(EvenNumbers(array));
+Console.WriteLine();
